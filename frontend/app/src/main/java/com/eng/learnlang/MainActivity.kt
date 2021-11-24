89a6d682788baeb8801b122a696b0774765ea041
Engin <enginakin6363@gmail.com>
2021-11-24 12:45:50 +0300

    a
    frontend

--- frontend/app/src/main/java/com/eng/learnlang/MainActivity.kt
+++ frontend/app/src/main/java/com/eng/learnlang/MainActivity.kt
@@ -3,10 +3,12 @@ package com.eng.learnlang
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.eng.learnlang.util.ui.theme.LearnlangTheme
 
@@ -16,8 +18,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnlangTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    
                 }
             }
         }
