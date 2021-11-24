4d27288bded5623381cbfa8a87221420b5e1d725
Engin <enginakin6363@gmail.com>
2021-11-24 14:38:26 +0300

    test for github

--- frontend/app/src/main/java/com/eng/learnlang/presentation/login/LoginScreen.kt
+++ frontend/app/src/main/java/com/eng/learnlang/presentation/login/LoginScreen.kt
@@ -13,7 +13,7 @@ fun LoginScreen(
     navController: NavController
 ) {
     Box(modifier = Modifier.fillMaxSize(),contentAlignment = Alignment.Center){
-        Text(text = "Login sayfası ")
+        Text(text = "Login değişiklik yapıldı Test değişikliği ")
     }
 
 }
